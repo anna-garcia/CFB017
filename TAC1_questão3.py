8c3ccb1dbefd827f8674ea40a44c60d458ddfc96
anna-garcia <70608263+anna-garcia@users.noreply.github.com>
2020-09-14 16:20:47 -0300

    Add files via upload

---
+++ TAC1_questão3.py
@@ -0,0 +1,11 @@
+import csv
+with open("/home/carol/Documents/CFB017/species.csv") as csvfile:
+# open(“caminho completo para o arquivo que vai ser analisado”)   
+    all_species = csv.reader(csvfile, delimiter=',')
+# leitura e identificação do separador (já age como um “split(,)”)
+    for lines in all_species:
+        if lines[3].upper().rstrip() == "BIRD":
+# a função .upper() deixa os caracteres da string em caixa alta.
+# a função .rstrip() remove espaços em branco (default) no final da string.
+            print(*lines,sep="\t")
+# o operador '*' retorna apenas os elementos da lista 'lines' com tab como separador.

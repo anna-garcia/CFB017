6302164c83c248479c896d7f7da0c690a4a34a8c
anna-garcia <70608263+anna-garcia@users.noreply.github.com>
2020-09-15 18:49:40 -0300

    Update TAC1_questão2.py

--- TAC1_questão2.py
+++ TAC1_questão2.py
@@ -1,23 +1,24 @@
-refArquivo = open("/home/carol/Documents/CFB017/TriTrypDB_Proteins.fasta")
 # open(“caminho completo para o arquivo que vai ser analisado”)
+refArquivo = open("/home/carol/Documents/CFB017/TriTrypDB_Proteins.fasta")
+# criação de variáveis
 cabeçalho = ""
 sequência = ""
-# criação de variáveis
-for line in refArquivo.readlines():
 # o readlines() gera uma LISTA com todas as linhas
-# line é uma variável que a cada loop recebe como valor uma lista que contém uma linha do arquivo
+# line é uma variável que a cada loop recebe como valor uma lista que contém uma linha do arquivo.
+for line in refArquivo.readlines():
     if ">" in line:
-        cabeçalho = line.strip('\n')
 # adiciona a linha atual à variável
 # o .rstrip('\n') deleta quebras de linha no final da string.
-# sem o .rstrip, o print abaixo gera uma linha em branco entra o cabeçalho e a sequencia
+# sem o .strip, o print abaixo gera uma linha em branco entra o cabeçalho e a sequencia
+        cabeçalho = line.strip('\n')
         print(cabeçalho)
+# perguntar se sequência não está vazia
         if sequência != "":
-# nesse caso, é o mesmo que perguntar se sequência não está vazia
-            sequência = ""
 # a variável sequência deve ser zerada dentro do loop para não acumular sequencias de outros cabeçalhos
+            sequência = ""
     else:
+# concatenação de sequências de AA
+# deleção das quebras de linha (\n)
         sequência += line.strip('\n')
         print(sequência)
-# deleção das quebras de linha (\n)
 refArquivo.close()

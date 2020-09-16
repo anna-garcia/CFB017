bbaf270091c198dd716b6e0386077068d7439451
anna-garcia <70608263+anna-garcia@users.noreply.github.com>
2020-09-16 12:46:03 -0300

    Update TAC1_questão2.py

--- TAC1_questão2.py
+++ TAC1_questão2.py
@@ -6,19 +6,22 @@ sequência = ""
 # o readlines() gera uma LISTA com todas as linhas
 # line é uma variável que a cada loop recebe como valor uma lista que contém uma linha do arquivo.
 for line in refArquivo.readlines():
-    if ">" in line:
-# adiciona a linha atual à variável
-# o .rstrip('\n') deleta quebras de linha no final da string.
-# sem o .strip, o print abaixo gera uma linha em branco entra o cabeçalho e a sequencia
-        cabeçalho = line.strip('\n')
-        print(cabeçalho)
+# busca por cabeçalhos.
+    if ">" in line:       
 # perguntar se sequência não está vazia
         if sequência != "":
+            print(cabeçalho)
+            print(sequência)
 # a variável sequência deve ser zerada dentro do loop para não acumular sequencias de outros cabeçalhos
             sequência = ""
+# sem o .strip, o print abaixo gera uma linha em branco entra o cabeçalho e a sequencia
+# adiciona a linha atual à variável
+        cabeçalho = line.strip('\n')
     else:
 # concatenação de sequências de AA
 # deleção das quebras de linha (\n)
         sequência += line.strip('\n')
-        print(sequência)
+# precisa realizar o print novamente para imprimir os últimos itens
+print(cabeçalho)
+print(sequência)
 refArquivo.close()

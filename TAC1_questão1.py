b4ee4ec3d9c256ae737b1ccaa70da25c39f64606
anna-garcia <70608263+anna-garcia@users.noreply.github.com>
2020-09-16 08:44:49 -0300

    Update TAC1_questão1.py

--- TAC1_questão1.py
+++ TAC1_questão1.py
@@ -1,21 +1,22 @@
-refArquivo = open("/home/carol/Documents/CFB017/gabarito_gene")
 # open(“caminho completo para o arquivo que vai ser analisado”).
+refArquivo = open("/home/carol/Documents/CFB017/gabarito_gene")
+# criação de variáveis.
 cabeçalho = ""
 sequência = ""
-# criação de variáveis.
-for line in refArquivo.readlines():
 # o readlines() gera uma LISTA com todas as linhas.
+for line in refArquivo.readlines():
+# encontrar cabeçalho.
     if ">" in line:
-        gene_nome = line.split(':')[0]
 # o split vai fatiar a lista dentro da variável 'line' com o separador ':'. 
 # utilizamos apenas o primeiro item da lista([0]).
-        cabeçalho = gene_nome.replace(">",'')
+        gene_nome = line.split(':')[0]
 # adiciona a linha atual à variável.
 # o .replace deleta o caractére ‘>’.
+        cabeçalho = gene_nome.replace(">",'')
     else:
-        sequência += line.replace('\n',"")
 # adição em loop das linhas de sequencia (tem mais de uma por causa das quebras do texto).
 # line.replace retira as quebras de linha/new lines(\n).
-print(cabeçalho,'\t',sequência)
+        sequência += line.replace('\n',"")
 # ‘\t’ é o símbolo para a tabulação (tab) que age como separador das colunas.
+    print(cabeçalho,'\t',sequência)
 refArquivo.close()

f7ac3825f9ae9d8cc69f6e55add21f474d74a2d2
anna-garcia <70608263+anna-garcia@users.noreply.github.com>
2020-09-16 08:48:38 -0300

    Update TAC1_questão3.py

--- TAC1_questão3.py
+++ TAC1_questão3.py
@@ -1,12 +1,14 @@
+# importar biblioteca csv.
 import csv
+# open(“caminho completo para o arquivo que vai ser analisado”).
 with open("/home/carol/Documents/CFB017/species.csv") as csvfile:
-# open(“caminho completo para o arquivo que vai ser analisado”).   
-    all_species = csv.reader(csvfile, delimiter=',')
 # leitura e identificação do separador (já age como um “split(,)”).
+    all_species = csv.reader(csvfile, delimiter=',')
     for lines in all_species:
-        if lines[3].upper().rstrip() == "BIRD":
 # a função .upper() deixa os caracteres da string em caixa alta.
-# a função .rstrip() remove espaços em branco (default) no final da string.
-            print(*lines,sep="\t")
+# a função .rstrip() remove espaços em branco (default) no final da string.       
+        if lines[3].upper().rstrip() == "BIRD":
 # o operador '*' retorna apenas os elementos da lista 'lines'. 
 # sep="\t" define tab como separador.
+            print(*lines,sep="\t")
+

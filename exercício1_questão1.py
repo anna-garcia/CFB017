42afd52c91da3f1a01341a8207d7d1f8bf122dd0
anna-garcia <70608263+anna-garcia@users.noreply.github.com>
2020-09-17 20:35:33 -0300

    Add files via upload

---
+++ exercício1_questão1.py
@@ -0,0 +1,8 @@
+#importação da biblioteca para o script.
+import pandas
+# para o .read_excel poder rodar, o IDE exigiu o xlrd, um pacote do pandas para leitura de tabelas em excel (.xls ou .xlsx)
+import xlrd
+# leitura da tabela em formato excel.
+WHO_data = pandas.read_excel('/home/carol/Documents/CFB017/WHO.xls')
+# imprimir toda a tabela.
+print(WHO_data)

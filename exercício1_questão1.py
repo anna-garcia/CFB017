03c84748c9b0c4d0969b4d878c52d5327df0df74
anna-garcia <70608263+anna-garcia@users.noreply.github.com>
2020-09-17 20:36:18 -0300

    Update exercício1_questão1.py

--- exercício1_questão1.py
+++ exercício1_questão1.py
@@ -2,7 +2,7 @@
 import pandas
 # para o .read_excel poder rodar, o IDE exigiu o xlrd, um pacote do pandas para leitura de tabelas em excel (.xls ou .xlsx)
 import xlrd
-# leitura da tabela em formato excel.
+# leitura da tabela em formato excel. Coloque todo o caminho até o arquivo.
 WHO_data = pandas.read_excel('/home/carol/Documents/CFB017/WHO.xls')
 # imprimir toda a tabela.
 print(WHO_data)

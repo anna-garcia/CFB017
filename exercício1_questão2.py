54183da8dab25cf709f21fd604dd8613f4de9e07
anna-garcia <70608263+anna-garcia@users.noreply.github.com>
2020-09-17 21:47:52 -0300

    Update exercício1_questão2.py

--- exercício1_questão2.py
+++ exercício1_questão2.py
@@ -1,10 +1,10 @@
 # importação da biblioteca para o script.
 import pandas
-# para o .read_excel poder rodar, o IDE exigiu o xlrd, um pacote do pandas para leitura de tabelas em excel (.xls ou .xlsx)
+# para o .read_excel poder rodar, o IDE exigiu o xlrd, um pacote do pandas para leitura de tabelas em excel (.xls ou .xlsx).
 import xlrd
 # leitura da tabela em formato excel.
 WHO_data = pandas.read_excel('/home/carol/Documents/CFB017/WHO.xls')
-# criação de váriável
+# criação de váriável.
 # seleção da coluna que se quer somar.
 # .sum() realiza a soma e retorna o total.
 total_deaths = WHO_data['TB deaths'].sum()

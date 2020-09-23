b5087b94aee173ef71b76ff4e7a9a94328f36cec
anna-garcia <70608263+anna-garcia@users.noreply.github.com>
2020-09-23 19:20:16 -0300

    Add files via upload

---
+++ TAC2_Quantidade_Absoluta_Vitor.py
@@ -0,0 +1,19 @@
+## ITEM 1
+def quantidade_absoluta(coef_m, coef_b, tabela):
+# pra mim só funciona se eu imprtar pandas E pandas as pd.
+    import pandas
+    import pandas as pd
+    import xlrd
+## ITEM 2
+    df = pd.read_excel(tabela) 
+## ITEM 3
+    df_q = df[['Target_Name','Stage']]
+    df_q['Quantity'] = 10 ** ((df['CT']-coef_b)/coef_m)
+## ITEM 4
+    df_final = pd.merge(df,df_q)
+## ITEM 5
+    df_final.to_excel("/home/carol/Documents/CFB017/Tabela_Qntd_Abs.xlsx",sheet_name="CT_Abs")
+## ITEM 6
+    print(df_final)
+# para chamar a função utilize o comando abaixo sem as'#'
+# quantidade_absoluta(-3.397186047, 58.53223295, '/home/carol/Documents/CFB017/Valores_CT.xlsx')

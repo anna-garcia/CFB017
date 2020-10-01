f6790e42d9fb894e839b29d5600e351fc1cab031
anna-garcia <70608263+anna-garcia@users.noreply.github.com>
2020-10-01 14:08:39 -0300

    Add files via upload

---
+++ TAC2_Quantidade_Absoluta_Anna.py
@@ -0,0 +1,18 @@
+import sys
+import pandas
+import xlrd
+# para chamar no prompt de comando (terminal):
+tabela = sys.argv[1] #
+coef_m = float(sys.argv[2])
+coef_b = float(sys.argv[3])
+CT_values = pandas.read_excel(tabela)
+## adiciona uma nova coluna na tabela.
+## os valores dessa coluna serão as quantidades absolutas calculadas com a equação 2.    
+CT_values['Quantity'] = 10 ** ((CT_values['CT']-coef_b)/coef_m)
+## salvar aletrações em outro arquivo excel.
+CT_values.to_excel("/home/carol/Documents/CFB017/Tabela_Qntd_Abs_anna.xlsx",sheet_name="CT_Abs")
+## impressão da tabela
+print(CT_values)
+# para chamar a função no prompt digite no terminal:
+# python nome_desse_script.py caminho_até_a_sua_tabela coeficiente_m coeficiente_b
+# ex: python Calc_Quant_Abs.py ~/Documents/CFB017/Valores_CT.xlsx -3.4 58.5

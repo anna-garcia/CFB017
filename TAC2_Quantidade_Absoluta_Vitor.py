a5a44b4ff07e370f4f3c582c87cba6e244483dd7
anna-garcia <70608263+anna-garcia@users.noreply.github.com>
2020-10-01 13:27:32 -0300

    Update TAC2_Quantidade_Absoluta_Vitor.py

--- TAC2_Quantidade_Absoluta_Vitor.py
+++ TAC2_Quantidade_Absoluta_Vitor.py
@@ -1,19 +1,29 @@
 ## ITEM 1
-def quantidade_absoluta(tabela, coef_m, coef_b):
-# pra mim só funciona se eu imprtar pandas E pandas as pd.
-    import pandas
-    import pandas as pd
-    import xlrd
+# importação de bibliotecas
+# o meu apenas funciona se eu importar pandas E pandas as pd.
+import sys
+import pandas
+import pandas as pd
+import xlrd
+# para chamar no prompt de comando (terminal):
+tabela = sys.argv[1] #
+coef_m = float(sys.argv[2])
+coef_b = float(sys.argv[3])
 ## ITEM 2
-    df = pd.read_excel(tabela) 
+df = pd.read_excel(tabela) 
 ## ITEM 3
-    df_q = df[['Target_Name','Stage']]
-    df_q['Quantity'] = 10 ** ((df['CT']-coef_b)/coef_m)
+# criação de uma nova tabela:
+df_q = df[['Target_Name','Stage']]
+# adição de uma nova coluna:
+df_q['Quantity'] = 10 ** ((df['CT']-coef_b)/coef_m)
 ## ITEM 4
-    df_final = pd.merge(df,df_q)
+# juntar as tabelas (deleção das colunas iguais):
+df_final = pd.merge(df,df_q)
 ## ITEM 5
-    df_final.to_excel("/home/carol/Documents/CFB017/Tabela_Qntd_Abs.xlsx",sheet_name="CT_Abs")
+# salvar a nova tabela:
+df_final.to_excel("/home/carol/Documents/CFB017/Tabela_Qntd_Abs.xlsx",sheet_name="CT_Abs")
 ## ITEM 6
-    print(df_final)
-# para chamar a função utilize o comando abaixo sem as'#'
-# quantidade_absoluta('/home/carol/Documents/CFB017/Valores_CT.xlsx', -3.397186047, 58.53223295)
+print(df_final)
+# para chamar a função no prompt digite no terminal:
+# python nome_desse_script.py caminho_até_a_sua_tabela coeficiente_m coeficiente_b
+# ex: python Calc_Quant_Abs.py ~/Documents/CFB017/Valores_CT.xlsx -3.4 58.5

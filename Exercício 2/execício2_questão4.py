5b300cd5a8be6b92a9b22f7cbd78738a18c708d9
anna-garcia <70608263+anna-garcia@users.noreply.github.com>
2020-10-06 20:18:47 -0300

    Rename Execício2_Código 4.py to execício2_questão4.py

--- Exercício 2/execício2_questão4.py
+++ Exercício 2/execício2_questão4.py
@@ -1,14 +1,14 @@
-# recebendo sequência de pré-mRNA
-RNA = str(input('Digite sua sequência de RNA:'))
-# recebendo coordenadas exônicas
+# recebendo sequÃªncia de prÃ©-mRNA
+RNA = str(input('Digite sua sequÃªncia de RNA:'))
+# recebendo coordenadas exÃ´nicas
 ## o split separa o input e gera uma lista com os itens que foram separados
 coordenada_a = str(input('coordenada 1:')).split(';')
 coordenada_b = str(input('coordenada 2:')).split(';')
-# fatiando a string sequência
-## elementos da lista gerada pelo .split são transformadasem números
+# fatiando a string sequÃªncia
+## elementos da lista gerada pelo .split sÃ£o transformadasem nÃºmeros
 ## em python a contagem se inicia do zero, logo, deve-se subtrair 1 das coordenadas
 exon1 = RNA[int(coordenada_a[0])-1:int(coordenada_a[-1])-1]
 exon2 = RNA[int(coordenada_b[0])-1:int(coordenada_b[-1])-1]
-# concatenação dos exons
+# concatenaÃ§Ã£o dos exons
 mRNA = exon1+exon2
 print(mRNA)

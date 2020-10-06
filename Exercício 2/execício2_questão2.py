f70e0ccf668201b0caa28b4846cbe1192797de63
anna-garcia <70608263+anna-garcia@users.noreply.github.com>
2020-10-06 18:24:35 -0300

    Rename Execício2_Código 2.py to execício2_questão2.py

--- Exercício 2/execício2_questão2.py
+++ Exercício 2/execício2_questão2.py
@@ -1,13 +1,13 @@
-# recebendo a sequencia de AA do usuário
-seq = str(input('Sequência de AA:')).upper()
-# calculo do total de AA da sequência
+# recebendo a sequencia de AA do usuÃ¡rio
+seq = str(input('SequÃªncia de AA:')).upper()
+# calculo do total de AA da sequÃªncia
 total = len(seq)
-# dicionário com contador dos AA existentes
+# dicionÃ¡rio com contador dos AA existentes
 aa_count = { 'A': 0, 'C': 0, 'D' : 0,'E' : 0,'F' : 0,'G' : 0,'H' : 0,'I' : 0,'K' : 0,'L' : 0,'M' : 0,
 'N' : 0,'P' : 0,'Q' : 0,'R' : 0,'S' : 0,'T' : 0,'V' : 0,'W' : 0,'Y' : 0}
-# contagem pra cada aminoácido
+# contagem pra cada aminoÃ¡cido
 for i in seq:
-# a cada item da sequencia que estiver no dicionário, vai acrescentar  1 à contagem
+# a cada item da sequencia que estiver no dicionÃ¡rio, vai acrescentar  1 Ã  contagem
     aa_count[i] += 1
 # calculo da porcentagem
 for key , value in aa_count.items():

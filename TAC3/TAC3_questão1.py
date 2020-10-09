b56c992b38133d35c57f932f03bd9f17ee47979a
anna-garcia <70608263+anna-garcia@users.noreply.github.com>
2020-10-08 21:41:47 -0300

    Update TAC3_questão1.py

--- TAC3/TAC3_questão1.py
+++ TAC3/TAC3_questão1.py
@@ -1,15 +1,10 @@
 # importação de biblioteca:
 from Bio.Seq import Seq
 # recebendo a sequência de DNA como input
-## DNA = Seq(str(input('Insira sua sequência de DNA:')))
-### como arquivo FASTA:
-FASTA = open("/home/carol/Documents/CFB017/sequenciaDesconhecida.fasta")
-for lines in FASTA.readlines():
-    if ">" not in lines:
-        DNA = Seq(str(lines).rstrip('\n'))
+DNA = Seq(str(input('Insira sua sequência de DNA:')))
 # a função .transcribe realiza a transcrição
-        mRNA = DNA.transcribe()
+mRNA = DNA.transcribe()
 # a função .translate() realiza a tradução A PARTIR de um RNA
-        ptn = mRNA.translate()
+ptn = mRNA.translate()
 # imprimir cada sequência abaixo de um identificador
 print('mRNA:',mRNA,'ptn:',ptn,sep='\n')

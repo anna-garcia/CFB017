7559062a2687be277b6fa6a9ca8604d384ee6343
anna-garcia <70608263+anna-garcia@users.noreply.github.com>
2020-10-19 18:23:29 -0300

    Add files via upload

---
+++ Exercício 3/Exercício3_questão2.py
@@ -0,0 +1,19 @@
+#definindo função:
+def Aprovação(total_faltas, nota_1, nota_2, nota_3):
+#recebendo do usuário o nome do aluno:
+    nome = str(input("Nome Completo do Aluno:"))
+#calculando a porcentagem de faltas:
+    percent = float(100*(total_faltas/80))
+#calculando média:
+    média = float((nota_1+nota_2+nota_3)/3)
+#condições e situações de aprovação:
+    if percent <= 25 and média >= 7.0:
+        print("Nome: ",nome,"Média: ",média, "Faltas: ",percent, "Aprovado")
+    elif percent > 25:
+        print("Nome: ",nome,"Média: ",média, "Faltas: ",percent, "Reprovado por Falta")
+    elif média <= 7.0:
+        print("Nome: ",nome,"Média: ",média, "Faltas: ",percent, "Reprovado por Média")
+    elif percent > 25 and média<= 7.0:
+        print("Nome: ",nome,"Média: ",média, "Faltas: ",percent, "Reprovado por Falta e Média")
+#para chamar a função: 
+#Aprovação(total_faltas, nota_1, nota_2, nota_3)

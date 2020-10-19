7a249066c949c2fbcaa7c456d2ab24aa2fda30f6
anna-garcia <70608263+anna-garcia@users.noreply.github.com>
2020-10-18 21:18:28 -0300

    Update TAC3_questão3.py

--- TAC3/TAC3_questão3.py
+++ TAC3/TAC3_questão3.py
@@ -4,15 +4,15 @@ import pandas as pd
 from Bio.Seq import Seq
 from Bio import SeqIO
 from Bio.Blast.Applications import NcbiblastxCommandline
-# blastx path:
+# BLASTX path:
 blastx = "/home/carol/anaconda3/bin/blastx"
-# query:
-seq_hipot = r"/home/carol/Documents/CFB017/sequenciaDesconhecida.fasta"
-# Subject:
-Tcruzi_data = r"/home/carol/Documents/CFB017/TriTrypDB-47_Tcruzi_AnnotatedProteins.fasta"
+# Query: "/home/carol/Documents/CFB017/sequenciaDesconhecida.fasta"
+seq_hipot = input(str('Sequencia desconhecida: '))
+# Subject: "/home/carol/Documents/CFB017/TriTrypDB-47_Tcruzi_AnnotatedProteins.fasta"
+Tcruzi_data = input(str('Tcruzi Data: '))
 # Resultado:
 Blast_TAC3 = r"/home/carol/Documents/CFB017/Blast_TAC3.txt"
-# blast x
+# BLASTX
 meu_blast = NcbiblastxCommandline(cmd = blastx ,query = seq_hipot, subject = Tcruzi_data, evalue = 0.05, outfmt = 6, out = Blast_TAC3)
 # redirecionando resultados: 
 stdout, stdeer = meu_blast()
